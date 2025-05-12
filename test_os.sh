912bde35a2dc4dfc7ddf21a495836033c2801b94
school_vm <school_vm@42.com>
2025-05-12 14:12:36 +0200

    added comments

--- test_os.sh
+++ test_os.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-#Petit script qui permet de tester le iso de l'os
+#petit script qui permet de tester le iso de l'os
 
 ISO_PATH="../myos.iso"
 
-# Vérifie si le fichier ISO existe
+# verifie si le fichier ISO existe
 if [ ! -f "$ISO_PATH" ]; then
     echo "Erreur : L'ISO $ISO_PATH n'existe pas."
     echo "Veux-tu le construire ? Lance 'make all' d'abord."
     exit 1
 fi
 
-# Lance QEMU avec ton ISO
+# lance QEMU avec ton ISO
 echo "Lancement de ton OS dans QEMU..."
 qemu-system-i386 -cdrom "$ISO_PATH" -boot d -m 512M -vga std
 
